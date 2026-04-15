492aac1e51dd3768c346a76bf881aafd31c87a31
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-04-15 17:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let timerDisplay = document.getElementById('timer');
+let currentDate = new Date();
+
+setInterval(
+	function () {
+
+    currentDate = new Date(); 
+
+    timerDisplay.innerHTML = currentDate.toLocaleString();
+
+}, 1000);
